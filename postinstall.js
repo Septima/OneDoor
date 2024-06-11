e7ea50f41ed12312242e0f183faa364a1a46bf65
Martin Bjærge Jensen <martin@septima.dk>
2024-06-11 10:36:54 +0200

    postinstall script

---
+++ postinstall.js
@@ -0,0 +1,6 @@
+console.log('postinstall: is working')
+// Skal kun ske første gang
+// 1. kopier config config folder fra node_modules og læg i roden af site
+// 2. Skab web.config og sæt CONFIG_PATH samt log path til iismodule
+// 
+
