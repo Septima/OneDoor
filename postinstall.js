8b59d36ea67184f364c309dfbc43928f45adab27
Martin Bjærge Jensen <martin@septima.dk>
2024-11-07 15:10:20 +0100

    v 1.21.1

--- postinstall.js
+++ postinstall.js
@@ -23,60 +23,66 @@ else {
         }
       });
     }
-
-   let web_config = `<?xml version="1.0" encoding="UTF-8"?>
-<configuration>
-    <appSettings>
-        <add key="CONFIG_PATH" value="${root}\config" />
-    </appSettings>
-    <system.webServer>
-        <iisnode
-            node_env="%node_env%"
-            nodeProcessCountPerApplication="1"
-            maxConcurrentRequestsPerProcess="1024"
-            maxNamedPipeConnectionRetry="100"
-            namedPipeConnectionRetryDelay="250"
-            maxNamedPipeConnectionPoolSize="512"
-            maxNamedPipePooledConnectionAge="30000"
-            asyncCompletionThreadCount="0"
-            initialRequestBufferSize="4096"
-            maxRequestBufferSize="65536"
-            watchedFiles="*.yml"
-            uncFileChangesPollingInterval="5000"
-            gracefulShutdownTimeout="60000"
-            loggingEnabled="true"
-            logDirectory="${root}\log"
-            debuggingEnabled="true"
-            debugHeaderEnabled="false"
-            debuggerPortRange="5058-6058"
-            debuggerPathSegment="debug"
-            maxLogFileSizeInKB="128"
-            maxTotalLogFileSizeInKB="1024"
-            maxLogFiles="20"
-            devErrorsEnabled="true"
-            flushResponse="false"
-            enableXFF="false"
-            promoteServerVars="LOGON_USER"
-            configOverrides="iisnode.yml"
-        />
-        <handlers>
-            <add name="iisnode" path="node_modules/@septima/onedoor-server/index.cjs" verb="*"
-                modules="iisnode" />
-        </handlers>
-        <rewrite>
-            <rules>
-                <rule name="onedoor-server">
-                    <match url="/*" />
-                    <action type="Rewrite" url="node_modules/@septima/onedoor-server/index.cjs" />
-                </rule>
-            </rules>
-        </rewrite>
-    </system.webServer>
-</configuration>` 
-fs.writeFile(root+'/web.config', web_config, err => {
-    if (err) {
-        console.error(err);
-    } else {
-        // file written successfully
+    if (fs.existsSync(root+'/web.config') ) {
+        console.log('Has already web.config . Exiting..')
     }
-    });
+    else {
+        console.log('creating NEW web.config ..')
+   let web_config = `<?xml version="1.0" encoding="UTF-8"?>
+   <configuration>
+       <appSettings>
+           <add key="CONFIG_PATH" value="${root}\config" />
+       </appSettings>
+       <system.webServer>
+           <iisnode
+               node_env="%node_env%"
+               nodeProcessCountPerApplication="1"
+               maxConcurrentRequestsPerProcess="1024"
+               maxNamedPipeConnectionRetry="100"
+               namedPipeConnectionRetryDelay="250"
+               maxNamedPipeConnectionPoolSize="512"
+               maxNamedPipePooledConnectionAge="30000"
+               asyncCompletionThreadCount="0"
+               initialRequestBufferSize="4096"
+               maxRequestBufferSize="65536"
+               watchedFiles="*.yml"
+               uncFileChangesPollingInterval="5000"
+               gracefulShutdownTimeout="60000"
+               loggingEnabled="true"
+               logDirectory="${root}\log"
+               debuggingEnabled="true"
+               debugHeaderEnabled="false"
+               debuggerPortRange="5058-6058"
+               debuggerPathSegment="debug"
+               maxLogFileSizeInKB="128"
+               maxTotalLogFileSizeInKB="1024"
+               maxLogFiles="20"
+               devErrorsEnabled="true"
+               flushResponse="false"
+               enableXFF="false"
+               promoteServerVars="LOGON_USER"
+               configOverrides="iisnode.yml"
+           />
+           <handlers>
+               <add name="iisnode" path="node_modules/@septima/onedoor-server/index.cjs" verb="*"
+                   modules="iisnode" />
+           </handlers>
+           <rewrite>
+               <rules>
+                   <rule name="onedoor-server">
+                       <match url="/*" />
+                       <action type="Rewrite" url="node_modules/@septima/onedoor-server/index.cjs" />
+                   </rule>
+               </rules>
+           </rewrite>
+       </system.webServer>
+   </configuration>` 
+   fs.writeFile(root+'/web.config', web_config, err => {
+       if (err) {
+           console.error(err);
+       } else {
+           // file written successfully
+       }
+       });
+   
+    }
